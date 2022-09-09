390f1268fab700650c300cc92b5025b507bf1e9a
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-09-09 17:14:19 +0200

    Added user input for numbers of squares

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 
 const WIDTH = 600;
 const container = document.querySelector(".container");
-for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
+const grid = +prompt("Squares per side?", "1");
+
+for(let i = 0; i < grid; i++){
+    for(let j = 0; j < grid; j++){
         const square = document.createElement("div");
         square.classList.add("square");
-        square.style.width = `calc(${WIDTH}px / 16)`;
-        square.style.height = `calc(${WIDTH}px / 16)`;
+        square.style.width = `calc(${WIDTH}px / ${grid})`;
+        square.style.height = `calc(${WIDTH}px / ${grid})`;
 
         container.appendChild(square);
     }
