4d711f3a4e1cfbc976f305c3a402a4126633873b
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-09-10 12:47:30 +0200

    Deleted clear() and implemented user input that change grid

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const WIDTH = 550;
 const container = document.querySelector(".container");
 container.style.width = `${WIDTH}px`;
 container.style.height = `${WIDTH}px`;
-let grid = 16;
+const grid = 16;
+makeGrid(grid);
+colorSquares();
 
-// const grid = +prompt("Squares per side?", "1");
 const slider = document.querySelector(".slider__input");
 const slider__label = document.querySelector(".slider__label");
 
@@ -14,29 +15,35 @@ slider__label.textContent = slider.value;
 
 slider.addEventListener("input", () => {
     slider__label.textContent = slider.value;
-    clear();
+    makeGrid(slider.value);
+    colorSquares();
 });
 
-for(let i = 0; i < grid; i++){
-    for(let j = 0; j < grid; j++){
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.style.width = `calc(${WIDTH}px / ${grid})`;
-        square.style.height = `calc(${WIDTH}px / ${grid})`;
-
-        container.appendChild(square);
+function makeGrid(grid){
+    container.innerHTML = "";
+    for(let i = 0; i < grid; i++){
+        for(let j = 0; j < grid; j++){
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.width = `calc(${WIDTH}px / ${grid})`;
+            square.style.height = `calc(${WIDTH}px / ${grid})`;
+    
+            container.appendChild(square);
+        }
     }
 }
 
-const squares = document.querySelectorAll(".square");
-squares.forEach(square => {
-    square.addEventListener("mouseover", () => {
-        square.classList.add("color");
-    })
-});
-
-function clear(){
-    squares.forEach(square => square.classList.remove("color"));
+function colorSquares(){
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener("mouseover", () => {
+            square.classList.add("color");
+        })
+    });
 }
+
+// function clear(){
+//     squares.forEach(square => square.classList.remove("color"));
+// }
 
 
